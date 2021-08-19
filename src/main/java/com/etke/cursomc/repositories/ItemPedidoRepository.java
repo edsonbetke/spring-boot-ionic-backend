43c1508db9a493c33e8002c26921864530767bcb
edsonbetke <edsonb@gmail.com>
2021-08-19 15:27:31 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/etke/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.etke.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.etke.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
