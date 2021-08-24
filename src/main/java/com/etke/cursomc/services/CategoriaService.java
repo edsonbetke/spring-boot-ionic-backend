8328946eaf65dd18b8668f3bf25c5a84c4f79fb2
edsonbetke <edsonb@gmail.com>
2021-08-24 20:01:34 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/etke/cursomc/services/CategoriaService.java
+++ src/main/java/com/etke/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.etke.cursomc.domain.Categoria;
+import com.etke.cursomc.dto.CategoriaDTO;
 import com.etke.cursomc.repositories.CategoriaRepository;
 import com.etke.cursomc.services.exceptions.DataIntegrityException;
 import com.etke.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
