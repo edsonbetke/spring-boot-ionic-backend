a4cc571c7dc485500dd3b78facdfecf13eb37678
edsonbetke <edsonb@gmail.com>
2021-09-09 13:26:30 -0300

    Email HTML

--- src/main/java/com/etke/cursomc/services/EmailService.java
+++ src/main/java/com/etke/cursomc/services/EmailService.java
@@ -1,13 +1,18 @@
 package com.etke.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.etke.cursomc.domain.Pedido;
 
 public interface EmailService {
 
-	
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
