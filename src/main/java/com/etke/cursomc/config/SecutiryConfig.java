7a2873d220eba03eeb27c47334e798541606e763
edsonbetke <edsonb@gmail.com>
2021-09-16 12:32:36 -0300

    Implementando autorização

--- src/main/java/com/etke/cursomc/config/SecutiryConfig.java
+++ src/main/java/com/etke/cursomc/config/SecutiryConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.etke.cursomc.security.JWTAuthenticationFilter;
+import com.etke.cursomc.security.JWTAuthorizationFilter;
 import com.etke.cursomc.security.JWTUtil;
 
 @Configuration
@@ -46,9 +47,12 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
